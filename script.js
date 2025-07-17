af7668a6a596569ce8dce73de03086a30081e1d9
Petrus Juan Pradipta Raindarta <petrusju4n@gmail.com>
2025-07-18 03:12:18 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Landing page Dusun Wonosobo 1 siap digunakan.");
+});
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".custom-navbar");
+  const carousel = document.querySelector("#carouselExampleAutoplaying");
+
+  // Dapatkan posisi bagian bawah carousel relatif terhadap viewport
+  const carouselBottom = carousel.getBoundingClientRect().bottom;
+
+  if (carouselBottom <= 0) {
+    // Jika carousel sudah sepenuhnya lewat, ubah navbar jadi transparan
+    navbar.classList.add("transparent");
+  } else {
+    // Jika masih di dalam carousel, tetap gelap
+    navbar.classList.remove("transparent");
+  }
+});
+
+console.log("carouselBottom:", carouselBottom, "scrollY:", window.scrollY);
+
+
