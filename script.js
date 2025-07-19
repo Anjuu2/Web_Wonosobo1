4396ac03eadc42069b7d0c7cb7f47c2c3dad1358
Petrus Juan Pradipta Raindarta <petrusju4n@gmail.com>
2025-07-20 02:58:57 +0700

    tambah maps, animasi, whatsapp

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+AOS.init();
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Landing page Dusun Wonosobo 1 siap digunakan.");
 });
@@ -7,7 +9,7 @@ window.addEventListener("scroll", function () {
   const carousel = document.querySelector("#carouselExampleAutoplaying");
 
   // Dapatkan posisi bagian bawah carousel relatif terhadap viewport
-  const carouselBottom = carousel.getBoundingClientRect().bottom;
+  const carouselBottom = carousel.getBoundingClientRect().bottom-70;
 
   if (carouselBottom <= 0) {
     // Jika carousel sudah sepenuhnya lewat, ubah navbar jadi transparan
